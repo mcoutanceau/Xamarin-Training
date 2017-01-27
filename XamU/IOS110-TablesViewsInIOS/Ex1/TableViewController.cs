2db1eebe34b06b9cd3c0a69678b9b4d1f3827308
Marc-André COUTANCEAU <ma.coutanceau@ideine.fr>
2017-01-27 11:08:18 +0100

    IOS110 Ex2, implementation of firsts steps of the exercice (Using the UITableViewController to populate a TableView)

--- XamU/IOS110-TablesViewsInIOS/Ex1/TableViewController.cs
+++ XamU/IOS110-TablesViewsInIOS/Ex1/TableViewController.cs
@@ -1,4 +1,6 @@
+using CoreGraphics;
 using Foundation;
+using Mailbox;
 using System;
 using UIKit;
 
@@ -6,8 +8,23 @@ namespace MailBox
 {
     public partial class TableViewController : UITableViewController
     {
+        private readonly EmailServer _emailServer = new EmailServer();
+
         public TableViewController (IntPtr handle) : base (handle)
         {
+
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return _emailServer.Email.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = new UITableViewCell(CGRect.Empty);
+            cell.TextLabel.Text = _emailServer.Email[indexPath.Row].Subject;
+            return cell;
         }
     }
 }
