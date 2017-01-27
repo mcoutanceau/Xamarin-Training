5ef00749aa0d1d54e39e6a647a36ff2b8aaa17cc
Marc-André COUTANCEAU <ma.coutanceau@ideine.fr>
2017-01-27 10:21:05 +0100

    IOS110 Ex1, adding new table view controller using the designer

---
+++ XamU/IOS110-TablesViewsInIOS/Ex1/TableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MailBox
+{
+    public partial class TableViewController : UITableViewController
+    {
+        public TableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
