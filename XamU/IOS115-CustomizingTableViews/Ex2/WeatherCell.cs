d7de7be5f4ee8049547e38b8902166d822e4936f
Marc-André COUTANCEAU <ma.coutanceau@ideine.fr>
2017-01-30 11:08:39 +0100

    IOS115, Ex2 Implementation, creating custom cells

---
+++ XamU/IOS115-CustomizingTableViews/Ex2/WeatherCell.cs
@@ -0,0 +1,49 @@
+﻿using CoreGraphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+
+namespace WeatherApp
+{
+    internal class WeatherCell : UITableViewCell
+    {
+        private readonly UILabel     _lblCity    = new UILabel();
+        private readonly UIImageView _imgWeather = new UIImageView();
+        private readonly UILabel     _lblHigh    = new UILabel();
+        private readonly UILabel     _lblLow     = new UILabel();
+
+        public WeatherCell(IntPtr handle) : base(handle)
+        {
+            this.ContentView.AddSubviews(_lblCity, _imgWeather, _lblHigh, _lblLow);
+        }
+        //public WeatherCell(IntPtr handle) : base(handle)
+        //{
+        //    this.ContentView.AddSubviews(_lblCity, _imgWeather, _lblHigh, _lblLow);
+        //}
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+            var width = ContentView.Frame.Width;
+            _imgWeather.Frame  = new CGRect(0, 0, 30, 30);
+            _imgWeather.Center = this.ContentView.Center;
+
+            _lblCity.Frame = new CGRect(20, 7, 100, 30);
+            _lblLow.Frame  = new CGRect(width - 50, 7, 30, 30);
+            _lblHigh.Frame = new CGRect(_lblLow.Frame.Left - 30, 7, 30, 30);
+        }
+
+        public void UpdateData(Weather pWeather)
+        {
+            if (pWeather == null)
+                throw new ArgumentNullException(nameof(pWeather));
+            this._lblCity.Text      = pWeather.City;
+
+            this._imgWeather.Image?.Dispose();
+            this._imgWeather.Image  = UIImage.FromBundle(pWeather.CurrentConditions.ToString() + ".png");
+            this._lblHigh.Text      = pWeather.High.ToString();
+            this._lblLow.Text       = pWeather.Low.ToString();
+        }
+    }
+}
--- XamU/IOS115-CustomizingTableViews/Ex2/WeatherTVC.cs
+++ XamU/IOS115-CustomizingTableViews/Ex2/WeatherTVC.cs
@@ -9,29 +9,31 @@ namespace WeatherApp
 	partial class WeatherTVC : UITableViewController
 	{
 		const string CELL_ID = "cell_id";
-		List<Weather> data;
+		List<Weather> _data;
 
 		public WeatherTVC (IntPtr handle) : base (handle)
 		{
-			data = WeatherFactory.GetWeatherData ();
-		}
+			_data = WeatherFactory.GetWeatherData ();
+            this.TableView.RegisterClassForCellReuse(typeof(WeatherCell), CELL_ID);
+        }
 
-		public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
 		{
-			var cell = tableView.DequeueReusableCell (CELL_ID);
-
-			if (cell == null) {
-				cell = new UITableViewCell (UITableViewCellStyle.Default, CELL_ID);
-			}
-
-			cell.TextLabel.Text = data [indexPath.Row].ToString ();
-
-			return cell;
+            return this.GetWeatherCell(tableView, indexPath);
 		}
+
+        public WeatherCell GetWeatherCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = (WeatherCell)tableView.DequeueReusableCell(CELL_ID);
+            if (cell == null)
+                cell = new WeatherCell(base.Handle);
+            cell.UpdateData(_data[indexPath.Row]);
+            return cell;
+        }
 
 		public override nint RowsInSection (UITableView tableview, nint section)
 		{
-			return data.Count;
+			return _data.Count;
 		}
 	}
 }
