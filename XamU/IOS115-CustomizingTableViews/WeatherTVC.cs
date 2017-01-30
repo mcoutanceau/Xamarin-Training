fe61b9f435a144998b2fb62aa6c0154592fcf397
Marc-André COUTANCEAU <ma.coutanceau@ideine.fr>
2017-01-30 09:18:53 +0100

    Implementation of IOS115 Ex 1

--- XamU/IOS115-CustomizingTableViews/WeatherTVC.cs
+++ XamU/IOS115-CustomizingTableViews/WeatherTVC.cs
@@ -9,25 +9,31 @@ namespace WeatherApp
 	partial class WeatherTVC : UITableViewController
 	{
 		const string CELL_ID = "id";
-		List<Weather> data;
+		private readonly List<Weather> data;
 
 		public WeatherTVC (IntPtr handle) : base (handle)
 		{
-			data = WeatherFactory.GetWeatherData ();
-
-			TableView.ContentInset = new UIEdgeInsets (this.TopLayoutGuide.Length, 0, 0, 0);
+			data = WeatherFactory.GetWeatherData();
+			TableView.ContentInset = new UIEdgeInsets(this.TopLayoutGuide.Length, 0, 0, 0);
 		}
 
 		public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
 		{
 			var cell = tableView.DequeueReusableCell (CELL_ID);
 
-			if (cell == null) {
-				cell = new UITableViewCell (UITableViewCellStyle.Default, CELL_ID);
-			}
+            if (cell == null)
+            { 
+                cell = new UITableViewCell(UITableViewCellStyle.Subtitle, CELL_ID);
+                cell.TextLabel.TextColor = UIColor.FromRGB(59, 102, 136);
+                cell.DetailTextLabel.TextColor = UIColor.FromRGB(0, 142, 255);
+            }
+            else
+                cell.ImageView.Image?.Dispose();
 
-			cell.TextLabel.Text = data [indexPath.Row].ToString ();
-
+            var weatherData = data[indexPath.Row];
+            cell.TextLabel.Text = weatherData.City;
+            cell.DetailTextLabel.Text = weatherData.ToString();
+            cell.ImageView.Image = UIImage.FromBundle(weatherData.CurrentConditions + ".png");
 			return cell;
 		}
 
@@ -35,10 +41,5 @@ namespace WeatherApp
 		{
 			return data.Count;
 		}
-
-
-
-
-
 	}
 }
