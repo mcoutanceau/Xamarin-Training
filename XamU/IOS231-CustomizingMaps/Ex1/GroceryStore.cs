058a64b15d09cc5f88c550dbec24a42db538f0c3
Marc-AndréCOUTANCEAU <Marc-AndrCOUTANCEAU@DESKTOP-RKR3VLM>
2017-02-02 16:02:19 +0100

    Adding pristine IOS231 Exercice 1

---
+++ XamU/IOS231-CustomizingMaps/Ex1/GroceryStore.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BananaFinder
+{
+	public class GroceryStore
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+
+		public double TimeOpen { get; set; }
+		public double TimeClosed { get; set; }
+
+		public GroceryStore ()
+		{
+		}
+	}
+
+	public class StoreFactory 
+	{
+		static List<GroceryStore> stores;
+
+		public static List<GroceryStore> GetStores ()
+		{
+			if (stores != null)
+				return stores;
+
+			stores = new List<GroceryStore> ();
+
+			stores.Add (new GroceryStore() {Name = "Dave's Grocery", Description = "Great groceries and a great price", Latitude = 49.28, Longitude=-123.11, TimeOpen = 9, TimeClosed = 5});
+				
+			stores.Add (new GroceryStore() {Name = "Mary's Produce", Description = "The finest produce this side of the Rockies", Latitude = 49.28, Longitude=-123.12, TimeOpen = 10, TimeClosed = 6});
+
+			stores.Add (new GroceryStore() {Name = "Fruitland", Description = "We do fruit, only better", Latitude = 49.28, Longitude=-123.13, TimeOpen = 8, TimeClosed = 4});
+
+			stores.Add (new GroceryStore() {Name = "Wonder Veggies", Description = "Never compromise on the quality of your veggies", Latitude = 49.275, Longitude=-123.11, TimeOpen = 8, TimeClosed = 8});
+	
+			stores.Add (new GroceryStore() {Name = "Bananarama", Description = "The biggest banana selection in the west", Latitude = 49.275, Longitude=-123.12, TimeOpen = 10, TimeClosed = 10});
+
+			stores.Add (new GroceryStore() {Name = "Quicky Veggy", Description = "Get your shopping done - quick", Latitude = 49.275, Longitude=-123.13, TimeOpen = 0, TimeClosed = 24});
+
+			return stores;
+		}
+	}
+}
+
---
+++ XamU/IOS231-CustomizingMaps/Ex1/ViewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using UIKit;
+using MapKit;
+using CoreLocation;
+
+namespace BananaFinder
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			//position the map over downtown Vancouver
+			map.Camera.CenterCoordinate = new CLLocationCoordinate2D (49.28275, -123.12);
+			map.Camera.Altitude = 10000;
+		}
+	}
+}
+
