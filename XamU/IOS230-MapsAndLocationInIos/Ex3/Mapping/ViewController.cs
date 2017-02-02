fed4c0ab0c98a35521ee2af09080703137ab8104
Marc-AndréCOUTANCEAU <Marc-AndrCOUTANCEAU@DESKTOP-RKR3VLM>
2017-02-02 14:49:10 +0100

    IOS230, Exercice 3 implementation, set camera with animation and test pitched view

--- XamU/IOS230-MapsAndLocationInIos/Ex3/Mapping/ViewController.cs
+++ XamU/IOS230-MapsAndLocationInIos/Ex3/Mapping/ViewController.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using UIKit;
-using CoreLocation;
+﻿using CoreLocation;
 using Foundation;
 using MapKit;
+using System;
+using UIKit;
 
 namespace Mapping
 {
-	public partial class ViewController : UIViewController
+    public partial class ViewController : UIViewController
 	{
-        CLLocationManager locMan = new CLLocationManager();
+        private readonly CLLocationManager      _locMan       = new CLLocationManager();
+        private readonly CLLocationCoordinate2D _locSanFran   = new CLLocationCoordinate2D(37.7833, -122.4167);
+        private readonly CLLocationCoordinate2D _locBoston    = new CLLocationCoordinate2D(42.3601, -71.0589);
+        private readonly CLLocationCoordinate2D _locLondon    = new CLLocationCoordinate2D(51.5072, -0.1275);
+        private readonly CLLocationCoordinate2D _locSingapore = new CLLocationCoordinate2D(1.3, 103.8);
 
-		public ViewController (IntPtr handle) : base (handle)
+        public ViewController (IntPtr handle) : base (handle)
 		{
 		}
 
 		public override void ViewDidLoad ()
 		{
-			base.ViewDidLoad ();
-			// Perform any additional setup after loading the view, typically from a nib.
-
-			if (UIDevice.CurrentDevice.CheckSystemVersion (8, 0) == true) {
-				locMan.RequestWhenInUseAuthorization ();
-			}
-
+			base.ViewDidLoad();// Perform any additional setup after loading the view, typically from a nib.
+            if (UIDevice.CurrentDevice.CheckSystemVersion (8, 0))
+				_locMan.RequestWhenInUseAuthorization ();
 			map.ShowsUserLocation = true;
 		}
 
-     	public override void ViewDidAppear(bool animated)
+        public override void ViewDidAppear(bool animated)
         {
           	base.ViewDidAppear(animated);
 
@@ -63,24 +63,47 @@ namespace Mapping
 
 		partial void btnSanFran_Activated (UIBarButtonItem sender)
 		{
-
+            map.Camera.Altitude  = 1000;
+            map.CenterCoordinate = _locSanFran;
+            map.MapType = MKMapType.Standard;
+            map.Camera.Pitch = 45;
+            map.ShowsBuildings = true;
+            map.PitchEnabled = true;
 		}
 
 		partial void btnBoston_Activated (UIBarButtonItem sender)
 		{
-			
-		}
+            map.Camera.Altitude = 1000;
+            map.CenterCoordinate = _locBoston;
+            map.MapType = MKMapType.Standard;
+            map.Camera.Pitch = 45;
+            map.ShowsBuildings = true;
+            map.PitchEnabled = true;
+        }
 
 		partial void btnLondon_Activated (UIBarButtonItem sender)
 		{
-
-		}
+            var oldCam = map.Camera;
+            var newCam = new MKMapCamera();
+            newCam.CenterCoordinate = _locLondon;
+            newCam.Altitude = 10000;
+            newCam.Pitch = 45;
+            newCam.Heading = 180;
+            map.Camera = newCam;
+            oldCam.Dispose(); //Dispose nécessaire ?
+        }
 
 		partial void btnSingapore_Activated (UIBarButtonItem sender)
 		{
-
-
-		}
+            var newCam = new MKMapCamera();
+            newCam.CenterCoordinate = _locSingapore;
+            newCam.Altitude = 10000;
+            newCam.Pitch = 45;
+            newCam.Heading = 180;
+            var oldCam = map.Camera;
+            map.SetCamera(newCam, true);
+            oldCam.Dispose(); //Dispose nécessaire ?
+        }
 	}
 }
 
