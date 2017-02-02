0fd577d9a29343f16f0b7145de881cec18967c63
Marc-AndréCOUTANCEAU <Marc-AndrCOUTANCEAU@DESKTOP-RKR3VLM>
2017-02-02 15:30:37 +0100

    IOS230, Exercice 4 Add an annotation to your map

--- XamU/IOS230-MapsAndLocationInIos/Ex4/Mapping/ViewController.cs
+++ XamU/IOS230-MapsAndLocationInIos/Ex4/Mapping/ViewController.cs
@@ -10,34 +10,39 @@ namespace Mapping
 {
 	public partial class ViewController : UIViewController
 	{
-        CLLocationManager locMan = new CLLocationManager();
-        
-		CLLocationCoordinate2D locSanFran = new CLLocationCoordinate2D(37.7833, -122.4167);
-		CLLocationCoordinate2D locBoston = new CLLocationCoordinate2D(42.3601, -71.0589);
-		CLLocationCoordinate2D locLondon = new CLLocationCoordinate2D(51.5072, -0.1275);
-		CLLocationCoordinate2D locSingapore = new CLLocationCoordinate2D(1.3, 103.8);
+        private readonly CLLocationManager      _locMan       = new CLLocationManager();
+		private readonly CLLocationCoordinate2D _locSanFran   = new CLLocationCoordinate2D(37.7833, -122.4167);
+		private readonly CLLocationCoordinate2D _locBoston    = new CLLocationCoordinate2D(42.3601, -71.0589);
+		private readonly CLLocationCoordinate2D _locLondon    = new CLLocationCoordinate2D(51.5072, -0.1275);
+		private readonly CLLocationCoordinate2D _locSingapore = new CLLocationCoordinate2D(1.3, 103.8);
 
 		public ViewController (IntPtr handle) : base (handle)
 		{
 		}
 
+        public void AddAnnotation(CLLocationCoordinate2D pCoord, string pTitle, string pSubtitle)
+        {
+            map.AddAnnotation(new MKPointAnnotation()
+            {
+                Coordinate = pCoord,
+                Title = pTitle,
+                Subtitle = pSubtitle
+            });
+        }
+
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
-
-			if (UIDevice.CurrentDevice.CheckSystemVersion (8, 0) == true) {
-				locMan.RequestWhenInUseAuthorization ();
-			}
-
+			if (UIDevice.CurrentDevice.CheckSystemVersion (8, 0))
+				_locMan.RequestWhenInUseAuthorization ();
 			map.MapType = MKMapType.Hybrid;
 			map.ShowsUserLocation = true;
 		}
 
 		public override void ViewDidAppear(bool animated)
         {
-          	base.ViewDidAppear(animated);
-
+            base.ViewDidAppear(animated);
             if (CLLocationManager.Status == CLAuthorizationStatus.Denied)
             {
                 if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0) == true)
@@ -71,13 +76,23 @@ namespace Mapping
 
         partial void btnPoints_Activated (UIBarButtonItem sender)
         {
-            // TODO:
+            this.AddAnnotation(_locSanFran, "If you are going to...", "don't forget to put some flowers in your hair.");
+            this.AddAnnotation(_locBoston, "Boston", "Business and leisure at the same time.");
+            this.AddAnnotation(_locLondon, "London...", "...calling");
+            this.AddAnnotation(_locSingapore, "Singapore", "Welcome to the Lion City");
+            btnAddPoints.Enabled = false;
+            btnRemovePoints.Enabled = true;
         }
 
 		partial void btnRemove_Activated (UIBarButtonItem sender)
 		{
-            // TODO:
-		}
+            IMKAnnotation[] annotations;
+            this.map.RemoveAnnotations(annotations = this.map.Annotations);
+            foreach (var annotation in annotations)
+                annotation.Dispose();//Dispose necessaire ?
+            btnAddPoints.Enabled    = true;
+            btnRemovePoints.Enabled = false;
+        }
 	}
 }
 
