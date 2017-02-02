d6e38c39d777f23d6a0b1d8dab71508e42f4f120
Marc-AndréCOUTANCEAU <Marc-AndrCOUTANCEAU@DESKTOP-RKR3VLM>
2017-02-02 17:21:34 +0100

    Implementation of IOS231 Exercice 2, Create a custom annotation

--- XamU/IOS231-CustomizingMaps/Ex1/GroceryMapDelegate.cs
+++ XamU/IOS231-CustomizingMaps/Ex1/GroceryMapDelegate.cs
@@ -7,11 +7,33 @@ namespace BananaFinder
     {
         public override MKAnnotationView GetViewForAnnotation(MKMapView mapView, IMKAnnotation pAnnotation)
         {
+            return GetViewForEx2(mapView, pAnnotation);
+        }
+
+        /// <summary>
+        /// Shows pins, color is changing depending on the TimeOpen property.
+        /// </summary>
+        private static MKAnnotationView GetViewForEx1(MKMapView mapView, IMKAnnotation pAnnotation)
+        {
             StoreAnnotation annotation = (StoreAnnotation)pAnnotation;
             return new MKPinAnnotationView(annotation, reuseIdentifier: "pin")
             {
-                PinTintColor = (annotation.TimeOpen < 9) ? UIColor.Purple : UIColor.Gray
+                PinTintColor = (annotation.TimeOpen < 9) ? UIColor.Purple : UIColor.Gray//Having a dynamic color may be note suitable with a reuseIdentifier.
             };
+        }
+
+        /// <summary>
+        /// Shows annotation with a banana picture.
+        /// </summary>
+        private static MKAnnotationView GetViewForEx2(MKMapView mapView, IMKAnnotation pAnnotation)
+        {
+            const string reuseIdentifier = "pin";
+            return mapView.DequeueReusableAnnotation(reuseIdentifier)
+                ?? new MKAnnotationView(pAnnotation, reuseIdentifier)
+                {
+                    Image = UIImage.FromBundle("banana_pin.png"),
+                    CenterOffset = new CoreGraphics.CGPoint(0, -20)
+                };
         }
     }
 }
